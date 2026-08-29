3057aaf2cbd40bfc816e51a824b37dc34e65ba74
Ry Bealey <hello@ryanbealey.com>
2026-08-29 15:29:57 -0500

    Revert "feat: send players into the game client after login"
    This reverts commit 765d532dba8303cf2ee50d6adba32111f109710f.

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -12,7 +12,7 @@ test('users can authenticate using the login screen', function () {
 
     expect($response->status())->toBe(302)
         ->and(auth()->check())->toBeTrue()
-        ->and(parse_url($response->headers->get('Location'), PHP_URL_PATH))->toBe('/game/nitro');
+        ->and(parse_url($response->headers->get('Location'), PHP_URL_PATH))->toBe('/user/me');
 });
 
 test('users can not authenticate with invalid password', function () {
