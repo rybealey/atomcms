d57a011e5bd41048552c8f757448663099acacde
Ryan Bealey <hello@ryanbealey.com>
2026-09-04 21:05:53 -0500

    style(client): page title reads "PixelRP: San Francisco"

--- resources/themes/pixelrp/views/client/nitro.blade.php
+++ resources/themes/pixelrp/views/client/nitro.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex, nofollow">
 
-    <title>{{ setting('hotel_name') }} - Nitro</title>
+    <title>{{ setting('hotel_name') }}: San Francisco</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
 
